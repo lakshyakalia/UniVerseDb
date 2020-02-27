ca3140224816ea3a64abcab13fdde490265f88bf
himanshu1365 <himanshu1365.cse16@chitkara.edu.in>
2020-02-27 19:21:44 +0530

    made some changes

--- Frontend/tailored-brand-poc/src/app/purchase-order/purchase-dialog-box.component.ts
+++ Frontend/tailored-brand-poc/src/app/purchase-order/purchase-dialog-box.component.ts
@@ -5,7 +5,7 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 	selector: 'dialog-box',
 	styles:['.heading{font-weight:100; font-size: 18px}','button{background-color: #77cc6d; color : white; font-weight: 500; margin-left: 90%; padding-left: 18px; padding-right:18px; border-radius : 4px; border:none ; }'],
 	templateUrl: './purchase-dialog-box.component.ts',
-	template:'<span class="heading">Congrats ! {{ data.msg }}</span><div><button (click)="closeDialogBox()">Ok</button></div>'
+	template:'<span class="heading">{{ data.msg }}</span><div><button (click)="closeDialogBox()">Ok</button></div>'
 	
 })
 export class PurchaseDialogBoxComponent{
