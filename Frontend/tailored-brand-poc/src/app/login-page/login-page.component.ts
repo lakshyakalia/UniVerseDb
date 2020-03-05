6eaecc3075a289efb2c5cdbc7447e6fbeefb17d9
lakshyakalia <lkalia.cse16@chitkarauniversity.edu.in>
2020-03-05 16:17:04 +0530

    login component validation added , service made and api called

---
+++ Frontend/tailored-brand-poc/src/app/login-page/login-page.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder,  Validators } from '@angular/forms';
+import{Router} from '@angular/router'
+import { LoginService } from '../service/login.service'
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+  loginForm: FormGroup;
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+
+  ) { }
+
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      username: new FormControl('',[Validators.required]),
+      password: new FormControl('',[Validators.required])
+    })
+    if (localStorage.getItem("token") === null) {
+    }
+  }
+  checkValidation() {
+    let status = true
+    if (this.loginForm.invalid) {
+      this.loginForm.get('username').markAsTouched()
+      this.loginForm.get('password').markAsTouched()
+
+      status = false
+    }
+    return status
+  }
+  login(){
+    if(!this.checkValidation())
+    {
+      return
+    }
+    else {
+      this.loginService.login(this.loginForm.value)
+      .subscribe((res) => {
+        console.log(res)
+      })
+    }
+  }
+
+}
